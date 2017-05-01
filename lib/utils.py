619838beb3efb078d36820083499a4cdda503a74
dinggul <dinggul@kaist.ac.kr>
2017-05-02 00:31:49 +0900

    Update customizing

--- lib/utils.py
+++ lib/utils.py
@@ -110,6 +110,7 @@ def colorize(text, color=None, attrib=None):
     ref: https://github.com/hellman/libcolors/blob/master/libcolors.py
     """
     # ansicolor definitions
+    attrib = 'light'
     COLORS = {"black": "30", "red": "31", "green": "32", "yellow": "33",
                 "blue": "34", "purple": "35", "cyan": "36", "white": "37"}
     CATTRS = {"regular": "0", "bold": "1", "underline": "4", "strike": "9",
